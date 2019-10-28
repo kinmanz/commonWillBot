778e4fa61f6923b42d6d4bbf6c4b187623eb9836
kinmanz <luga2012@yandex.ru>
2019-10-28 22:52:43 +0300

    final_refactor

--- helpers.py
+++ helpers.py
@@ -1,2 +1,37 @@
 def log(*messages):
+    # TODO Real logging
     print(*messages)
+
+
+class PollStat:
+
+    def __str__(self):
+        return f"PollStat[{self.likes}, {self.dislikes}, {self.rejects}] Stiker = {self.stiker_id}"
+
+    def __repr__(self):
+        return str(self)
+
+    def __init__(self, stiker_id):
+        self.likes = set()
+        self.dislikes = set()
+        self.rejects = set()
+        self.stiker_id = stiker_id
+
+    def remove_vote(self, user_id):
+        self.likes.discard(user_id)
+        self.dislikes.discard(user_id)
+        self.rejects.discard(user_id)
+
+    def add_like(self, user_id):
+        self.likes.add(user_id)
+
+    def add_dislike(self, user_id):
+        self.dislikes.add(user_id)
+
+    def add_reject(self, user_id):
+        self.rejects.add(user_id)
+
+    def __iter__(self):
+        yield self.likes
+        yield self.dislikes
+        yield self.rejects
--- willBot.py
+++ willBot.py
@@ -5,15 +5,15 @@ import telebot
 import telebot.apihelper
 from telebot import types
 
-from helpers import log
-from property import EMOJI
+from helpers import log, PollStat
+from property import EMOJI, DELETE_MESSAGE_COUNT, MINIMUM_COUNT
 from property import PRP
 
 LAST_WILL_USERS_FILE = 'last_will.pickle'
 
 bot = telebot.TeleBot(PRP.BOT_TOKEN, threaded=False)
 
-polling_status = {}
+TRACKED_POLLS = {}
 
 last_will_users = set()
 
@@ -40,15 +40,20 @@ def is_member(chat_id: str, user_id: str):
     except Exception as exp:
         log("Not a member tries to access bot", user_id, exp)
         log(chat_id, user_id)
-        return False
+    return False
+
+
+def send_stiker(chat_id, stiker_name):
+    sti = open(f"stikers/{stiker_name}.webp", 'rb')
+    return bot.send_sticker(chat_id, sti)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    sti = open('stikers/ready_to_work.webp', 'rb')
-    bot.send_sticker(message.chat.id, sti)
-    bot.reply_to(message, "Готов к работе. Напиши сообщение с тегом #aloud_in_private "
-                          "в самом начале, и я отправлю его в студенческий чат для голосования.")
+    if in_private(message):
+        send_stiker(message.chat.id, "ready_to_work")
+        bot.reply_to(message, "Готов к работе. Напиши сообщение с тегом #aloud_in_private "
+                              "в самом начале, и я отправлю его в студенческий чат для голосования.")
 
 
 @bot.message_handler(regexp='^#last_will\s(\w|\W)*')
@@ -80,99 +85,79 @@ def aloud_in_private(message):
     user_id = message.from_user.id
     if in_private(message) and is_member(PRP.STUDENT_CHAT_ID, user_id):
         text = message.html_text[len('#aloud_in_private'):]
-        sti = open('stikers/claim_accepted.webp', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        send_stiker(message.chat.id, "claim_accepted")
         bot.reply_to(message, "Ваше обращение принято.")
         publish_claim_to_chat(text)
     else:
         bot.reply_to(message, "Эй, такое только в личку!")
 
 
-MINIMUM_COUNT = 2  # TODO 15
-DELETE_MESSAGE_COUNT = 2  # TODO 7
-PUBLISH_MESSAGE_CONT = 10
-MINIMUM_COUNT_TO_POST = 2  # TODO 15
+def handle_poll(message, message_id) -> bool:
+    stat: PollStat = TRACKED_POLLS[message_id]
+    likes, dislikes, hates = stat
 
+    should_delete = False
 
-def handle_pool(message, message_id):
-    polling = polling_status[message_id]
-    likes = polling[0]
-    dislikes = polling[1]
-    hates = polling[2]
-    if len(likes) + len(dislikes) + len(hates) >= MINIMUM_COUNT:
-        if len(likes) >= len(dislikes) + len(hates):
-            text = message.message.html_text
-            sti = open('stikers/common_chat_publish.webp', 'rb')
-            bot.send_sticker(PRP.COMMON_CHAT, sti)
-            bot.send_message(PRP.COMMON_CHAT, "<b>Люди выразили своё мнение:</b> \n" + text, parse_mode="HTML")
-            bot.delete_message(PRP.STUDENT_CHAT_ID, message_id)
-            bot.delete_message(PRP.STUDENT_CHAT_ID, polling[3])
-            del polling_status[message_id]
-        elif len(hates) >= DELETE_MESSAGE_COUNT:
-            bot.delete_message(PRP.STUDENT_CHAT_ID, message_id)
-            bot.delete_message(PRP.STUDENT_CHAT_ID, polling[3])
-            del polling_status[message_id]
+    if len(likes) + len(dislikes) + len(hates) < MINIMUM_COUNT:
+        return should_delete
+
+    should_publish = len(likes) > len(dislikes) + len(hates)
+    should_delete = len(hates) >= DELETE_MESSAGE_COUNT or should_publish
+
+    if should_publish:
+        text = message.message.html_text
+        send_stiker(PRP.COMMON_CHAT, "common_chat_publish")
+        bot.send_message(PRP.COMMON_CHAT, "<b>Люди выразили своё мнение:</b> \n" + text, parse_mode="HTML")
+
+    if should_delete:
+        bot.delete_message(PRP.STUDENT_CHAT_ID, message_id)
+        bot.delete_message(PRP.STUDENT_CHAT_ID, stat.stiker_id)
+        del TRACKED_POLLS[message_id]
+    return should_delete
 
 
 def publish_claim_to_chat(text):
-    markup = types.InlineKeyboardMarkup()
-    btn_my_site = types.InlineKeyboardButton(EMOJI.THUMBS_UP + " 0", callback_data="LIKE")
-    btn_my_site1 = types.InlineKeyboardButton(EMOJI.THUMBS_DOWN + " 0", callback_data="HATE")
-    btn_my_site2 = types.InlineKeyboardButton(EMOJI.EYE + " 0", callback_data="NOT_KNOW")
+    sti_msg = send_stiker(PRP.STUDENT_CHAT_ID, "cries")
 
-    l = [btn_my_site, btn_my_site1, btn_my_site2]
-    markup.row(*l)
+    btn_my_site = types.InlineKeyboardButton(f"{EMOJI.THUMBS_UP} 0", callback_data="LIKE")
+    btn_my_site1 = types.InlineKeyboardButton(f"{EMOJI.THUMBS_DOWN} 0", callback_data="DISLIKE")
+    btn_my_site2 = types.InlineKeyboardButton(f"{EMOJI.REJECT} 0", callback_data="REJECT")
 
-    sti = open('stikers/cries.webp', 'rb')
-    sti_msg = bot.send_sticker(PRP.STUDENT_CHAT_ID, sti)
-
+    markup = types.InlineKeyboardMarkup().row(btn_my_site, btn_my_site1, btn_my_site2)
     message = bot.send_message(PRP.STUDENT_CHAT_ID, text, reply_markup=markup)
-    polling_status[message.message_id] = [set(), set(), set(), sti_msg.message_id]
+    TRACKED_POLLS[message.message_id] = PollStat(stiker_id=sti_msg.message_id)
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def test_callback(call):
-    markup = types.InlineKeyboardMarkup()
+def poll_vote_update(call):
     message_id = call.message.message_id
+
+    if message_id not in TRACKED_POLLS:
+        return
+
     user_id = call.from_user.id
     type_of_query = call.data
-    if message_id not in polling_status:
-        return
 
-    stat = polling_status[message_id]
+    stat: PollStat = TRACKED_POLLS[message_id]
+    stat.remove_vote(user_id)
 
     if type_of_query == "LIKE":
-        if user_id not in stat[0]:
-            stat[0].add(user_id)
-        else:
-            stat[0].remove(user_id)
-        stat[1].discard(user_id)
-        stat[2].discard(user_id)
-    elif type_of_query == "HATE":
-        if user_id not in stat[1]:
-            stat[1].add(user_id)
-        else:
-            stat[1].remove(user_id)
-        stat[0].discard(user_id)
-        stat[2].discard(user_id)
-    elif type_of_query == 'NOT_KNOW':
-        if user_id not in stat[2]:
-            stat[2].add(user_id)
-        else:
-            stat[2].remove(user_id)
-        stat[0].discard(user_id)
-        stat[1].discard(user_id)
-    else:
-        return
-    handle_pool(call, message_id)
-    btn_my_site = types.InlineKeyboardButton(f"{EMOJI.THUMBS_UP} {len(stat[0])}", callback_data="LIKE")
-    btn_my_site1 = types.InlineKeyboardButton(f"{EMOJI.THUMBS_DOWN} {len(stat[1])}", callback_data="HATE")
-    btn_my_site2 = types.InlineKeyboardButton(f"{EMOJI.EYE} {len(stat[2])}", callback_data="NOT_KNOW")
-    l = [btn_my_site, btn_my_site1, btn_my_site2]
-    markup.row(*l)
-    print(call)
+        stat.add_like(user_id)
+    elif type_of_query == "DISLIKE":
+        stat.add_dislike(user_id)
+    elif type_of_query == 'REJECT':
+        stat.add_reject(user_id)
 
+    if handle_poll(call, message_id):  # poll deleted
+        return
+
+    btn_my_site = types.InlineKeyboardButton(f"{EMOJI.THUMBS_UP} {len(stat.likes)}", callback_data="LIKE")
+    btn_my_site1 = types.InlineKeyboardButton(f"{EMOJI.THUMBS_DOWN} {len(stat.dislikes)}", callback_data="DISLIKE")
+    btn_my_site2 = types.InlineKeyboardButton(f"{EMOJI.REJECT} {len(stat.rejects)}", callback_data="REJECT")
+
+    markup = types.InlineKeyboardMarkup().row(btn_my_site, btn_my_site1, btn_my_site2)
     bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=message_id, reply_markup=markup)
+    # log(f"Callback : {call}")
 
 
 bot.polling(none_stop=True, interval=0)
