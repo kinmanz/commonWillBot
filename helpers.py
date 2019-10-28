4cdb7d79741cb7b7a75e2277682a13805712e76f
kinmanz <luga2012@yandex.ru>
2019-10-28 11:43:43 +0300

    refactor

---
+++ helpers.py
@@ -0,0 +1,2 @@
+def log(*messages):
+    print(*messages)
--- willBot.py
+++ willBot.py
@@ -1,17 +1,18 @@
+import telebot
 import telebot.apihelper
-from property import PRP
-from property import EMOJI
 from telebot import types
-import time
+
+from helpers import log
+from property import EMOJI
+from property import PRP
+
 bot = telebot.TeleBot(PRP.BOT_TOKEN, threaded=False)
 
 polling_status = {}
 
 
 def in_private(message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    return chat_id == user_id
+    return message.chat.id == message.from_user.id
 
 
 def is_member(chat_id: str, user_id: str):
@@ -20,15 +21,18 @@ def is_member(chat_id: str, user_id: str):
         status = ans['status']
         return status != 'left' and status != 'kicked'
     except Exception as exp:
-        # TODO логгировать юзера
-        print(exp)
-        print(chat_id, user_id)
+        log("Not a member tries to access bot", user_id, exp)
+        log(chat_id, user_id)
         return False
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    sti = open('stikers/ready_to_work.webp', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+    bot.reply_to(message, "Готов к работе. Напиши сообщение с тегом #aloud_in_private "
+                          "в самом начале, и я отправлю его в студенческий чат для голосования.")
+
 
 # TODO decorator
 @bot.message_handler(regexp='^#aloud_in_private\s(\w|\W)*')
@@ -36,15 +40,18 @@ def aloud_in_private(message):
     user_id = message.from_user.id
     if in_private(message) and is_member(PRP.STUDENT_CHAT_ID, user_id):
         text = message.html_text[len('#aloud_in_private'):]
-        bot.reply_to(message, "Твоё обращение принято")
+        sti = open('stikers/claim_accepted.webp', 'rb')
+        bot.send_sticker(message.chat.id, sti)
+        bot.reply_to(message, "Ваше обращение принято.")
         publish_claim_to_chat(text)
     else:
         bot.reply_to(message, "Эй, такое только в личку!")
 
-MINIMUM_COUNT = 1 #TODO 15
-DELETE_MESSAGE_COUNT = 2 #TODO 7
+
+MINIMUM_COUNT = 2  # TODO 15
+DELETE_MESSAGE_COUNT = 2  # TODO 7
 PUBLISH_MESSAGE_CONT = 10
-MINIMUM_COUNT_TO_POST = 2 # TODO 15
+MINIMUM_COUNT_TO_POST = 2  # TODO 15
 
 
 def handle_pool(message, message_id):
@@ -65,13 +72,13 @@ def publish_claim_to_chat(text):
     markup = types.InlineKeyboardMarkup()
     btn_my_site = types.InlineKeyboardButton(EMOJI.THUMBS_UP + " 0", callback_data="LIKE")
     btn_my_site1 = types.InlineKeyboardButton(EMOJI.THUMBS_DOWN + " 0", callback_data="HATE")
-    btn_my_site2 = types.InlineKeyboardButton(EMOJI.EYE + " 0", callback_data="NOT_KNOW")
+    btn_my_site2 = types.InlineKeyboardButton("НЕ ЗНАЮ 0", callback_data="NOT_KNOW")
 
     markup.add(btn_my_site)
     markup.add(btn_my_site1)
     markup.add(btn_my_site2)
     message = bot.send_message(PRP.STUDENT_CHAT_ID, text, reply_markup=markup)
-    polling_status[message.message_id] = [set(),set(),set()]
+    polling_status[message.message_id] = [set(), set(), set()]
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -118,11 +125,6 @@ def test_callback(call):
     print(call)
 
     bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=message_id, reply_markup=markup)
-
-
-@bot.message_handler(commands=['LIKE'])
-def url1(message):
-    print("1")
 
 
 bot.polling(none_stop=True, interval=0)
